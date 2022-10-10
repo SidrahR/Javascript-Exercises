0961ae80cfab711673ccd489c928cd57463e7130
sidrahrashid <sidrahr92@gmail.com>
2022-10-10 17:45:59 +0500

    udapted question 20

--- 20_list.js
+++ 20_list.js
@@ -1,3 +1,12 @@
 //Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
 
-var languages = ["Urdu", "English", "Arabic", "Chinese"];
+var my_arr = [
+  languages=["Urdu", "English", "Arabic", "Chinese"],
+  cities= ["Islamabad", "Karachi", "Lahore"],
+  countries=["Pakistan", "India", "Russia", "China"]
+];
+
+console.log(my_arr);
+console.log(my_arr[0]);
+console.log(my_arr[2]);
+console.log(my_arr[1][2]);
